99df79db519859ea037ce0c551b5b642cad3c930
JANSZUSZ <janusz.wasowicz.poczta@gmail.com>
2020-11-04 15:00:09 +0100

    Intial

---
+++ Menu.cs
@@ -0,0 +1,81 @@
+using System;
+
+
+namespace O.R.K.A._Project_ver._2._0
+{
+    public class Menu
+    {
+        public void _Menu()
+        {
+            Methods methods = new Methods();
+            
+            Console.ForegroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+
+                    Console.Clear();
+                    Console.WriteLine("                           777777777777777777     77777777777777       777777777");
+                    Console.WriteLine("                                 7777777          77777777777777       777      777");
+                    Console.WriteLine("                                 7777777          7777                 777      777");
+                    Console.WriteLine("                                 7777777          77777777777777       7777777777");
+                    Console.WriteLine("                                 7777777          77777777777777       7777777777");
+                    Console.WriteLine("                                 7777777          7777                 777      777");
+                    Console.WriteLine("                                 7777777          77777777777777       777      777");
+                    Console.WriteLine("                                 7777777          77777777777777       777777777");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("           7777777       777777777777777777     7777     7777     777777777         7777       77777777777");
+                    Console.WriteLine("         7777    777           7777777          7777     7777     77777777777       7777      7777     7777");
+                    Console.WriteLine("           7777                7777777          7777     7777     7777    7777      7777      7777     7777");
+                    Console.WriteLine("             7777              7777777          7777     7777     7777     7777     7777      7777     7777");
+                    Console.WriteLine("               7777            7777777          7777     7777     7777     7777     7777      7777     7777");
+                    Console.WriteLine("                7777           7777777          7777     7777     7777    7777      7777      7777     7777");
+                    Console.WriteLine("         777  7777             7777777          7777777777777     77777777777       7777      7777777777777");
+                    Console.WriteLine("           777777              7777777           77777777777      7777777777        7777       77777777777");
+
+
+                    Methods.Ent();
+                GG:
+                    Methods.Clear();
+
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    Console.WriteLine("                                 77777777777        77777777777        7777   7777        77777777");
+                    Console.WriteLine("                               777777777777777      777777777777       7777  7777        7777777777");
+                    Console.WriteLine("                              7777         7777     7777       777     7777 7777        7777    7777");
+                    Console.WriteLine("                              7777         7777     7777       777     77777777         7777    7777");
+                    Console.WriteLine("                              7777         7777     777777777777       77777777         777777777777");
+                    Console.WriteLine("                              7777         7777     7777777777         7777  7777       777777777777");
+                    Console.WriteLine("                               777777777777777      7777    77777      7777   7777      7777    7777");
+                    Console.WriteLine("                                 77777777777        7777     77777     7777    7777     7777    7777");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("            7777              7777               7777777777777777     77777777777777     777777777        77777     77777777777777");
+                    Console.WriteLine("            7777              7777               7777777777777777     77777777777777     777      777     77777     77777777777777");
+                    Console.WriteLine("            7777     7777     7777                     7777           7777               777      777     77777     7777          ");
+                    Console.WriteLine("            7777    777777    7777                     7777           77777777777777     7777777777       77777     77777777777777");
+                    Console.WriteLine("            7777   777  777   7777                     7777           77777777777777     7777777777       77777     77777777777777");
+                    Console.WriteLine("            7777  777    777  7777                     7777           7777               777      777     77777     7777          ");
+                    Console.WriteLine("            77777777       7777777                     7777           77777777777777     777      777     77777     77777777777777");
+                    Console.WriteLine("             77777          77777                      7777           77777777777777     777777777        77777     77777777777777");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("                   7777777         777    77777");
+                    Console.WriteLine("                  777   777        777   777  777");
+                    Console.WriteLine("                  777   777        777   77777777");
+                    Console.WriteLine("                  777   777        777   777  777");
+                    Console.WriteLine("                   7777777       777     777  777     [] [] []");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("                                         1. Nowa Gra                       2. Wczytaj gre  ");
+                    Console.WriteLine("");
+                    Console.WriteLine("                                         3. Ustawienia                     4. Dowidzenia   ");
+                    Console.WriteLine("");
+                    Console.ResetColor();
+
+        }
+    }
+}
